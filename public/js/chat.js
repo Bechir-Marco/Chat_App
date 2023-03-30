3d06a932779d07ec28b4227a125afa97ef09280d
Bechir-Marco <bechir.marko@gmail.com>
2023-03-30 14:42:51 +0100

    Styling chat App and Fixing the Geo API Bug

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,26 +1,68 @@
-const socket = io();
+const socket = io()
+
+// Elements
+const $messageForm = document.querySelector('#message-form')
+const $messageFormInput = $messageForm.querySelector('input')
+const $messageFormButton = $messageForm.querySelector('button')
+const $sendLocationButton = document.querySelector('#send-location')
+const $messages = document.querySelector('#messages')
+
+// Templates
+const messageTemplate = document.querySelector('#message-template').innerHTML
+const locationMessageTemplate = document.querySelector('#location-message-template').innerHTML
 
 socket.on('message', (message) => {
-  console.log(message);
-});
+    console.log(message)
+    const html = Mustache.render(messageTemplate, {
+        message: message.text,
+        createdAt: moment(message.createdAt).format('h:mm a')
+    })
+    $messages.insertAdjacentHTML('beforeend', html)
+})
 
-document.querySelector('#message-form').addEventListener('submit', (e) => {
-  e.preventDefault();
+socket.on('locationMessage', (message) => {
+    console.log(message)
+    const html = Mustache.render(locationMessageTemplate, {
+        url: message.url,
+        createdAt: moment(message.createdAt).format('h:mm a')
+    })
+    $messages.insertAdjacentHTML('beforeend', html)
+})
 
-  const message = e.target.elements.message.value;
+$messageForm.addEventListener('submit', (e) => {
+    e.preventDefault()
 
-  socket.emit('sendMessage', message);
-});
+    $messageFormButton.setAttribute('disabled', 'disabled')
 
-document.querySelector('#send-location').addEventListener('click', () => {
-  if (!navigator.geolocation) {
-    return alert('Geolocation is not supported by your browser.');
-  }
+    const message = e.target.elements.message.value
 
-  navigator.geolocation.getCurrentPosition((position) => {
-    socket.emit('sendLocation', {
-      latitude: position.coords.latitude,
-      longitude: position.coords.longitude,
-    });
-  });
-});
+    socket.emit('sendMessage', message, (error) => {
+        $messageFormButton.removeAttribute('disabled')
+        $messageFormInput.value = ''
+        $messageFormInput.focus()
+
+        if (error) {
+            return console.log(error)
+        }
+
+        console.log('Message delivered!')
+    })
+})
+
+$sendLocationButton.addEventListener('click', () => {
+    if (!navigator.geolocation) {
+        return alert('Geolocation is not supported by your browser.')
+    }
+
+    $sendLocationButton.setAttribute('disabled', 'disabled')
+
+    navigator.geolocation.getCurrentPosition((position) => {
+        socket.emit('sendLocation', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        }, () => {
+            $sendLocationButton.removeAttribute('disabled')
+            console.log('Location shared!')  
+        })
+    }, undefined, {enableHighAccuracy: true}, {timeout:10_000})
+})
