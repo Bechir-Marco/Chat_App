e240484243424a62a0d58da1f9e7a78b6f43973d
Bechir-Marco <bechir.marko@gmail.com>
2023-03-31 20:07:01 +0100

    Making messages appears to specific users room

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const Filter = require('bad-words')
 const app = express()
 const server = http.createServer(app)
 const io = socketio(server)
-
+const {addUser, removeUser, getUser, getUsersInRoom} = require('./utils/users')
 const port = process.env.PORT || 3000
 const publicDirectoryPath = path.join(__dirname, '../public')
 
@@ -15,34 +15,45 @@ app.use(express.static(publicDirectoryPath))
 io.on('connection', (socket) => {
     console.log('New WebSocket connection')
 
-    socket.on('join', ({ username, room }) => {
-        socket.join(room)
+    socket.on('join', (options, callback) => {
+        const { error, user } = addUser({ id: socket.id, ...options })
 
-        socket.emit('message', generateMessage('Welcome!'))
-        socket.broadcast.to(room).emit('message', generateMessage(`${username} has joined!`))
+        if (error) {
+            return callback(error)
+        }
 
-        // socket.emit, io.emit, socket.broadcast.emit
-        // io.to.emit, socket.broadcast.to.emit
+        socket.join(user.room)
+
+        socket.emit('message', generateMessage('Admin', 'Welcome!'))
+        socket.broadcast.to(user.room).emit('message', generateMessage('Admin', `${user.username} has joined!`))
+
+        callback()
     })
 
     socket.on('sendMessage', (message, callback) => {
+        const user = getUser(socket.id)
         const filter = new Filter()
 
         if (filter.isProfane(message)) {
             return callback('Profanity is not allowed!')
         }
 
-        io.to('Center City').emit('message', generateMessage(message))
+        io.to(user.room).emit('message', generateMessage(user.username, message))
         callback()
     })
 
     socket.on('sendLocation', (coords, callback) => {
-        io.emit('locationMessage', generateLocationMessage(`https://google.com/maps?q=${coords.latitude},${coords.longitude}`))
+        const user = getUser(socket.id)
+        io.to(user.room).emit('locationMessage', generateLocationMessage(user.username, `https://google.com/maps?q=${coords.latitude},${coords.longitude}`))
         callback()
     })
 
     socket.on('disconnect', () => {
-        io.emit('message', generateMessage('A user has left!'))
+        const user = removeUser(socket.id)
+
+        if (user) {
+            io.to(user.room).emit('message', generateMessage('Admin', `${user.username} has left!`))
+        }
     })
 })
 
@@ -50,7 +61,8 @@ server.listen(port, () => {
     console.log(`Server is up on port ${process.env.PORT}!`)
 })
 
-const {
-  generateMessage,
-  generateLocationMessage,
-} = require('./utils/messages');
+//Notes: 
+//1) broadcast method broadcasts a message to every connected client except the sender who just joined the room(in our case)
+//2) io.to.emit emits a message to everyone in the specific room
+//3) socket.broadcast.to.emit emits a message to everyone except the sender in a specific room
+//4) io.emit send a message to every connected client
