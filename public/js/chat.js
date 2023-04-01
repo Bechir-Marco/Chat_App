d016599647aa6561f898c59652c1debf991613df
Bechir-Marco <bechir.marko@gmail.com>
2023-04-01 18:08:37 +0100

    auto scrolling implementation

--- public/js/chat.js
+++ public/js/chat.js
@@ -18,8 +18,27 @@ const sidebarTemplate = document.querySelector('#sidebar-template').innerHTML;
 const { username, room } = Qs.parse(location.search, {
   ignoreQueryPrefix: true,
 });
-const autoscroll = () => { 
+const autoscroll = () => {
+  // New message element
+  const $newMessage = $messages.lastElementChild;
 
+  // Height of the new message
+  const newMessageStyles = getComputedStyle($newMessage);
+  const newMessageMargin = parseInt(newMessageStyles.marginBottom);
+  const newMessageHeight = $newMessage.offsetHeight + newMessageMargin;
+
+  // Visible height
+  const visibleHeight = $messages.offsetHeight;
+
+  // Height of messages container
+  const containerHeight = $messages.scrollHeight;
+
+  // How far have I scrolled?
+  const scrollOffset = Math.ceil($messages.scrollTop + visibleHeight);
+
+  if (containerHeight - newMessageHeight <= scrollOffset) {
+    $messages.scrollTop = $messages.scrollHeight;
+  }
 };
 socket.on('message', (message) => {
   console.log(message);
